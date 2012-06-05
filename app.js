d9e605ae1fec2c75124243038c0b361983b81629
David Caplan <dcaplan@gmail.com>
2012-06-05 14:53:10 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,40 @@
+var express = require("express"),
+    app     = express.createServer(),
+    io = require('socket.io').listen(app),
+    _ = require('underscore')._;
+
+var sockets = [];
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+app.post('/ring/:type', function (req, res) {
+    if (req.params.type === 'bell') {
+        _.each(sockets, function(socket) {
+            socket.emit('ring', { type: req.params.type });
+        });
+    }
+});
+
+app.configure(function(){
+  app.use(express.methodOverride());
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/static'));
+  app.use(express.errorHandler({
+    dumpExceptions: true,
+    showStack: true
+  }));
+  app.use(app.router);
+});
+
+io.configure(function () {
+    io.set("transports", ["xhr-polling"]);
+    io.set("polling duration", 10);
+});
+
+io.sockets.on('connection', function (socket) {
+    sockets.push(socket);
+});
+
+app.listen(8080);
